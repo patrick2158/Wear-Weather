91dc081d8f3ee364447ce2459646e225b2005fc6
patrick2158 <patrick40312158@gmail.com>
2018-03-30 15:59:12 +0900

    Delete app.firebase.config.ts

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyB9UXVrE-vws7cHNhwrRbTazJgATINZH00",
-    authDomain: "wear-weather-6981c.firebaseapp.com",
-    databaseURL: "https://wear-weather-6981c.firebaseio.com",
-    projectId: "wear-weather-6981c",
-    storageBucket: "wear-weather-6981c.appspot.com",
-    messagingSenderId: "1024895966079"
-  };
